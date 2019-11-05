f598e04d17af7ad491b1d92f4344374793542b52
Zoey <zoey.zyhu@gmail.com>
2019-11-05 08:44:19 -0500

    revised and added more exception

--- scraper.py
+++ scraper.py
@@ -2,7 +2,7 @@
 
 # ===============================================
 # purpose: check # presence of brands in Factiva
-# usage: ./batch.py -u username -p password
+# usage: ./scraper.py -u username -p password
 # date: Nov. 03, 2019
 # author: Zoey Hu
 # contact: zyhu@gatech.edu
@@ -16,6 +16,7 @@ from selenium.webdriver.support.ui import Select
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.common.exceptions import NoSuchElementException 
+from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.support import expected_conditions as EC
 from time import sleep
 import datetime
@@ -25,11 +26,15 @@ import argparse
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-u', 
-  help = 'username'
-  )
+	help = 'username'
+	)
 parser.add_argument('-p', 
-  help = 'password'
-  )
+	help = 'password'
+	)
+parser.add_argument('-nb', 
+	help = 'index of brand to start',
+	default = -999
+	)
 args = vars(parser.parse_args())
 
 
@@ -49,29 +54,29 @@ def LoadOutput(filename):
 		n = 0
 	return n
 
-def WaitUntil(xpath):
-	wait = WebDriverWait(driver, 32400)
+def WaitUntil(xpath, s):
+	wait = WebDriverWait(driver, s)
 	element = wait.until(
 		EC.element_to_be_clickable((By.XPATH, xpath))
 	)
 
 def Click(xpath):
-	WaitUntil(xpath)
+	WaitUntil(xpath, 50)
 	elem = driver.find_element_by_xpath(xpath)
 	elem.click()
 
 def SendKey(xpath, key):
-	WaitUntil(xpath)
+	WaitUntil(xpath, 50)
 	elem = driver.find_element_by_xpath(xpath)
 	elem.send_keys(key)
 
 def SelectOption(xpath, value):
-	WaitUntil(xpath)
+	WaitUntil(xpath, 50)
 	select = Select(driver.find_element_by_xpath(xpath))
 	select.select_by_value(value)
 
 def GetValue(xpath):
-	WaitUntil(xpath)
+	WaitUntil(xpath, 50)
 	elem = driver.find_element_by_xpath(xpath)
 	value = elem.text
 	return value
@@ -79,31 +84,46 @@ def GetValue(xpath):
 def Login(url, username, password, xpath_username, xpath_password,
 	xpath_submit, xpath):
 	driver.get(url)
-	WaitUntil(xpath_username)
-	SendKey(xpath_username, username)
-	SendKey(xpath_password, password)
-	Click(xpath_submit)
-	WaitUntil(xpath)
+	try: 
+		WaitUntil(xpath_username, 50)
+		SendKey(xpath_username, username)
+		SendKey(xpath_password, password)
+		Click(xpath_submit)
+		WaitUntil(xpath, 32400)
+	except TimeoutException:
+		print('No need to login from start. Refreshed!')
+
 
 def SearchOrRestart(xpath, url, username, password, xpath_username,
 	xpath_password, xpath_submit):
 	try:
 		Click(xpath)
-	except NoSuchElementException:
-		print('Out of time. Restart!')
-		Login(url, username, password, xpath_username, xpath_password,
-			xpath_submit, xpath)	
+	except TimeoutException:
+		print('Out of time. Refresh!')
+		try:
+			driver.get(url)
+			Click(xpath)
+		except TimeoutException:
+			print('Refresh not successful. Re-login!')
+			try:
+				Login(url, username, password, xpath_username,
+				 xpath_password, xpath_submit, xpath)
+			except:
+				print('Stucked. Help me Restart:)')
 
-def ScrapeOrSwitch(xpath1, xpath2):
+def ScrapeOrSwitch(xpath1, xpath2, url, signal):
 	try:
 		count = GetValue(xpath1)
-	except NoSuchElementException:
+	except TimeoutException:
+		print('No publications found. Switch to "All".')
 		try:
 			count = GetValue(xpath2)
-		except NoSuchElementException:
-			print ('No result for publications. Check!')
-			count = 0
-	return count
+		except TimeoutException:
+			print ('No result for publications. Something weired. Do it over!')
+			driver.get(url)
+			signal = False
+			count = -999
+	return count, signal
 
 
 
@@ -114,10 +134,14 @@ if __name__ == '__main__':
 	filename2 = 'count.csv'
 	nrow, ncol, brandid, brand, pbrand = LoadInput(filename1)
 	n = LoadOutput(filename2)
+
+	# set initiating point
+	nb = int('%s' % args['nb'])
+	if nb != -999:
+		n = nb
 	print('Start from %d-th brand.' % n)
 
-	# - set dates range
-	mm = list(range(1, 13))
+	# - set year range
 	yy = list(range(2001, 2012))
 
 	# - set driver
@@ -138,90 +162,100 @@ if __name__ == '__main__':
 	# - get results
 	for i in range(n,nrow):
 		print(datetime.datetime.now())
+		print(i)
 		print(brand[i], pbrand[i])
 		for y in yy:
 			print(y)
-			for m in mm:
-				f = open(filename2, 'a')
-				# - 1. click on search in the navigation menu
-				xpath_navi = '//*[@id="navmbm0"]/a'
-				SearchOrRestart(xpath_navi, url, username, password, 
-					xpath_username, xpath_password, xpath_submit)				
+			m = 1
+			while m <= 12:
+				try:
+					signal = True
+					f = open(filename2, 'a')
+					# - 1. click on search in the navigation menu
+					xpath_navi = '//*[@id="navmbm0"]/a'
+					SearchOrRestart(xpath_navi, url, username, password, 
+						xpath_username, xpath_password, xpath_submit)				
 
-				# - 2. click on "search form"
-				xpath_sform = '//*[@id="sfs"]/a'
-				SearchOrRestart(xpath_sform, url, username, password, 
-					xpath_username, xpath_password, xpath_submit)
+					# - 2. click on "search form"
+					xpath_sform = '//*[@id="sfs"]/a'
+					SearchOrRestart(xpath_sform, url, username, password, 
+						xpath_username, xpath_password, xpath_submit)
 
-				# - 3. enter search values
-				# -- 3.1 input brand to 'This exact phrase'
-				xpath_brand = '//*[@id="htx"]'
-				key_brand = "'"+brand[i]+"'"
-				SendKey(xpath_brand, key_brand)
+					# - 3. enter search values
+					# -- 3.1 input brand to 'This exact phrase'
+					xpath_brand = '//*[@id="htx"]'
+					key_brand = "'"+brand[i]+"'"
+					SendKey(xpath_brand, key_brand)
 
-				# -- 3.2 input parent brand to 'At least one of these words'
-				xpath_pbrand = '//*[@id="otx"]'
-				if pbrand[i] == 'P&G':
-					key_pbrand = "'"+pbrand[i]+"', "+"'"+"Procter & Gamble"+"'"
-				if pbrand[i] == 'JOHNSON AND JOHNSON':
-					key_pbrand = "'"+pbrand[i]+"', "+"'"+"JOHNSON & JOHNSON"+"'"
-				else:
-					key_pbrand = "'"+pbrand[i]+"'"
-				SendKey(xpath_pbrand, key_pbrand)
+					# -- 3.2 input parent brand to 'At least one of these words'
+					xpath_pbrand = '//*[@id="otx"]'
+					if pbrand[i] == 'P&G':
+						key_pbrand = "'"+pbrand[i]+"', "+"'"+"Procter & Gamble"+"'"
+					if pbrand[i] == 'JOHNSON AND JOHNSON':
+						key_pbrand = "'"+pbrand[i]+"', "+"'"+"JOHNSON & JOHNSON"+"'"
+					else:
+						key_pbrand = "'"+pbrand[i]+"'"
+					SendKey(xpath_pbrand, key_pbrand)
 
-				# -- 3.3 input dates to 'Date -> Enter date range'
-				# --- click and drop options of 'Date'
-				xpath_date = '//*[@id="dr"]'
-				Click(xpath_date)
+					# -- 3.3 input dates to 'Date -> Enter date range'
+					# --- click and drop options of 'Date'
+					xpath_date = '//*[@id="dr"]'
+					Click(xpath_date)
 
-				# --- select 'Enter date range'
-				xpath_sdate = '//*[@id="dr"]'
-				value_sdate = 'Custom'
-				SelectOption(xpath_sdate, value_sdate)
+					# --- select 'Enter date range'
+					xpath_sdate = '//*[@id="dr"]'
+					value_sdate = 'Custom'
+					SelectOption(xpath_sdate, value_sdate)
 
-				# --- enter values for 'from' and 'to'
-				xpath_frmonth= '//*[@id="frm"]'
-				xpath_frday = '//*[@id="frd"]'
-				xpath_fryear = '//*[@id="fry"]'
-				key_frmonth = str(m)
-				key_frday = '1'
-				key_fryear = str(y)
-				xpath_tomonth= '//*[@id="tom"]'
-				xpath_today = '//*[@id="tod"]'
-				xpath_toyear = '//*[@id="toy"]'
-				if m == 12:
-					key_tomonth = str(1)
-					key_today = '1'
-					key_toyear = str(y+1)
-				else:
-					key_tomonth = str(m+1)
-					key_today = '1'
-					key_toyear = str(y)					
-				SendKey(xpath_frmonth, key_frmonth)
-				SendKey(xpath_frday, key_frday)
-				SendKey(xpath_fryear, key_fryear)
-				SendKey(xpath_tomonth, key_tomonth)
-				SendKey(xpath_today, key_today)
-				SendKey(xpath_toyear, key_toyear)
+					# --- enter values for 'from' and 'to'
+					xpath_frmonth= '//*[@id="frm"]'
+					xpath_frday = '//*[@id="frd"]'
+					xpath_fryear = '//*[@id="fry"]'
+					key_frmonth = str(m)
+					key_frday = '1'
+					key_fryear = str(y)
+					xpath_tomonth= '//*[@id="tom"]'
+					xpath_today = '//*[@id="tod"]'
+					xpath_toyear = '//*[@id="toy"]'
+					if m == 12:
+						key_tomonth = str(1)
+						key_today = '1'
+						key_toyear = str(y+1)
+					else:
+						key_tomonth = str(m+1)
+						key_today = '1'
+						key_toyear = str(y)					
+					SendKey(xpath_frmonth, key_frmonth)
+					SendKey(xpath_frday, key_frday)
+					SendKey(xpath_fryear, key_fryear)
+					SendKey(xpath_tomonth, key_tomonth)
+					SendKey(xpath_today, key_today)
+					SendKey(xpath_toyear, key_toyear)
 
-				# - 4. submit
-				xpath_search = '//*[@id="btnSearchBottom"]'
-				elem = driver.find_element_by_xpath(xpath_search)
-				elem.click()
+					# - 4. submit
+					xpath_search = '//*[@id="btnSearchBottom"]'
+					elem = driver.find_element_by_xpath(xpath_search)
+					elem.click()
 
-				# - 5. get # publications
-				xpath1 = '//*[@id="headlineTabs"]/table[1]/tbody/tr/td/span[3]/a/span'
-				xpath2 = '//*[@id="headlineTabs"]/table[1]/tbody/tr/td/span[2]/a/span'
-				count = ScrapeOrSwitch(xpath1, xpath2)
-				print(count)
-				# - 6. write data
-				f.write('%d\t' % i)
-				f.write('%s\t' % brandid[i])
-				f.write('%s\t' % brand[i])
-				f.write('%s\t' % pbrand[i])
-				f.write('%d\t' % m)
-				f.write('%d\t' % y)
-				f.write('%s\n' % count)
-				f.close()
-				sleep(random.randint(1, 4))	
+					# - 5. get # publications
+					xpath1 = '//*[@id="headlineTabs"]/table[1]/tbody/tr/td/span[3]/a/span'
+					xpath2 = '//*[@id="headlineTabs"]/table[1]/tbody/tr/td/span[2]/a/span'
+					count, signal = ScrapeOrSwitch(xpath1, xpath2, url, signal)
+					if signal == True:
+						print(m, count)
+						# - 6. write data
+						f.write('%d\t' % i)
+						f.write('%s\t' % brandid[i])
+						f.write('%s\t' % brand[i])
+						f.write('%s\t' % pbrand[i])
+						f.write('%d\t' % m)
+						f.write('%d\t' % y)
+						f.write('%s\n' % count)
+						f.close()
+						sleep(random.randint(1, 4))	
+						m += 1
+				except TimeoutException:
+					print(m)
+					print('Time out. Do it over!')		
+
 	# driver.close()
